495d0a8890b916ff45293dce40d351696b8095d4
iinow <iinow@naver.com>
2019-08-27 14:38:04 +0900

    redirect uri cookie 에서 사용하지 않도록 수정

--- src/main/java/com/ha/security/handler/OAuth2AuthenticationSuccessHandler.java
+++ src/main/java/com/ha/security/handler/OAuth2AuthenticationSuccessHandler.java
@@ -41,21 +41,29 @@ public class OAuth2AuthenticationSuccessHandler extends SimpleUrlAuthenticationS
         }
 
         clearAuthenticationAttributes(request, response);
+//        for(Cookie cookie: request.getCookies()) {
+//        	if(cookie.getName().equals(HttpCookieOAuth2AuthorizationRequestRepository.REDIRECT_URI_PARAM_COOKIE_NAME)) {
+//        		cookie.setMaxAge(0);
+//        	}
+//        }
         getRedirectStrategy().sendRedirect(request, response, targetUrl);
     }
 
+    /**
+     * redirect 주소 쿠기에서 가져오지 않도록 수정처리함..
+     * */
     protected String determineTargetUrl(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
         Optional<String> redirectUri = CookieUtils.getCookie(request, HttpCookieOAuth2AuthorizationRequestRepository.REDIRECT_URI_PARAM_COOKIE_NAME)
                 .map(Cookie::getValue);
 
         if(redirectUri.isPresent() && !isAuthorizedRedirectUri(redirectUri.get())) {
-//            throw new BadRequestException("Sorry! We've got an Unauthorized Redirect URI and can't proceed with the authentication");
+            throw new RuntimeException("Sorry! We've got an Unauthorized Redirect URI and can't proceed with the authentication");
         }
-
+        
         String targetUrl = redirectUri.orElse(getDefaultTargetUrl());
+        String targetUrl2 = appConfig.getOauth2().getAuthorizedRedirectUris().get(0);//redirectUri.orElse(getDefaultTargetUrl());
         String token = tokenProvider.createToken(authentication);
-
-        return UriComponentsBuilder.fromUriString(targetUrl)
+        return UriComponentsBuilder.fromUriString(targetUrl2)
                 .queryParam("token", token)
                 .build().toUriString();
     }
